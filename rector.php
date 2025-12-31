b5bc0bdde1054abc2696ce2cdf343640a6d367b3
inoha_kudo <inoha1062@gmail.com>
2025-12-31 14:31:17 +0900

    [update] Rectorの導入

---
+++ rector.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use RectorLaravel\Set\LaravelLevelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+    ])
+    ->withSets([
+        LevelSetList::UP_TO_PHP_84,
+        LaravelLevelSetList::UP_TO_LARAVEL_120,
+    ]);
