inoha-kudo/dynasty-starter-kit

[update] モジュラモノリス対応

resources/js/app.tsx
@@ -8,7 +8,18 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+    resolve: (name) => {
+        if (name.includes('::')) {
+            const [module, page] = name.split('::');
+
+            return resolvePageComponent(
+                `../../vendor/dynasty/${module}/resources/js/pages/${page}.tsx`,
+                import.meta.glob('../../vendor/dynasty/*/resources/js/pages/**/*.tsx'),
+            );
+        } else {
+            return resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx'));
+        }
+    },
     setup({ el, App, props }) {
         const root = createRoot(el);
 

resources/views/app.blade.php
@@ -14,7 +14,14 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @if(str_contains($page['component'], '::'))
+            @php
+                [$module, $path] = explode('::', $page['component']);
+            @endphp
+            @vite(['resources/js/app.tsx', "vendor/dynasty/{$module}/resources/js/pages/{$path}.tsx"])
+        @else
+            @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @endif
         @inertiaHead
     </head>
     <body class="font-sans antialiased">

inoha-kudo/empire-starter-kit

resources/js/app.tsx
@@ -8,7 +8,18 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
-    resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+    resolve: (name) => {
+        if (name.includes('::')) {
+            const [module, page] = name.split('::');
+
+            return resolvePageComponent(
+                `../../vendor/empire/${module}/resources/js/pages/${page}.tsx`,
+                import.meta.glob('../../vendor/empire/*/resources/js/pages/**/*.tsx'),
+            );
+        } else {
+            return resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx'));
+        }
+    },
     setup({ el, App, props }) {
         const root = createRoot(el);
 

resources/views/app.blade.php
@@ -14,7 +14,14 @@
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
         @viteReactRefresh
-        @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @if(str_contains($page['component'], '::'))
+            @php
+                [$module, $path] = explode('::', $page['component']);
+            @endphp
+            @vite(['resources/js/app.tsx', "vendor/empire/{$module}/resources/js/pages/{$path}.tsx"])
+        @else
+            @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+        @endif
         @inertiaHead
     </head>
     <body class="font-sans antialiased">